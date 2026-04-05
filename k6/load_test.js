58825d08236ef5e5a51982f79e18c43218c505e8
vietbui99 <buiquocviet99@gmail.com>
2026-04-05 12:39:29 -0400

    fail on CI prevent green checks even with fail/errors

--- k6/load_test.js
+++ k6/load_test.js
@@ -1,10 +1,14 @@
 import http from "k6/http";
 import { check } from "k6";
 
+// Fail on CI no Green Checks
 export const options = {
   vus: 50,
-  vus: 500,
   duration: "15s",
+  thresholds: {
+    http_req_failed: ["rate<0.05"], // fail if error rate > 5%
+    http_req_duration: ["p(95)<3000"], // fail if p95 > 3s
+  },
 };
 
 // LOCAL TEST
