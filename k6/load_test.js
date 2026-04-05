3953365fdb48a6992abe9b68b88d6e772f0ceff0
vietbui99 <buiquocviet99@gmail.com>
2026-04-05 12:37:31 -0400

    fix workers and add package installations during CI runs

--- k6/load_test.js
+++ k6/load_test.js
@@ -2,14 +2,22 @@ import http from "k6/http";
 import { check } from "k6";
 
 export const options = {
+  vus: 50,
   vus: 500,
-  duration: "30s",
+  duration: "15s",
 };
 
+// LOCAL TEST
+// export default function () {
+//   const res = http.get("http://localhost:5000/urls/1");
+//   check(res, { "status is 200": (r) => r.status === 200 });
+//   if (res.status !== 200) {
+//     console.log(`Failed: ${res.status} ${res.body}`);
+//   }
+// }
+
+// CI TEST
 export default function () {
   const res = http.get("http://localhost:5000/urls/1");
   check(res, { "status is 200": (r) => r.status === 200 });
-  if (res.status !== 200) {
-    console.log(`Failed: ${res.status} ${res.body}`);
-  }
 }
